4c81b6b0346d791b7160b91eeb51aaa7bd205ba7
IndexOutOfBound <Weikai.zeng95@gmail.com>
2019-05-02 23:40:35 +1000

    finish the deploy of couchdb cluster

--- Docker/run-docker.sh
+++ Docker/run-docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-group-61-openrc.sh; ansible-playbook --fork 1 -i hosts -u ubuntu --ask-become-pass docker.yaml
+#used for first time
+. ./unimelb-comp90024-group-61-openrc.sh < OpenStack_API_key.text; ansible-playbook --fork 1 -u ubuntu --ask-become-pass docker.yaml
+
+
+# . ./unimelb-comp90024-group-61-openrc.sh < OpenStack_API_key.text; ansible-playbook -u ubuntu --ask-become-pass docker.yaml
