f190f96970663e921116bbf5d10988fa110d308a
IndexOutOfBound <Weikai.zeng95@gmail.com>
2019-05-07 00:52:39 +1000

    finish the couchdb part

--- CouchDB/command.sh
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-docker service create --name couchdb-master -p 8091:8091 --replicas 1 --network couchdb -e TYPE=MASTER arungupta/couchdb:swarm
-
-
-docker service create --name couchdb-worker --replicas 1 -e TYPE=WORKER -e COUCHBASE_MASTER=couchdb-master.couchdb --network couchdb arungupta/couchdb:swarm
